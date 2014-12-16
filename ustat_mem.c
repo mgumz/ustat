506fe023a730d66694e8376a246f9bb6b7cc0873
Mathias Gumz <mg@2hoch5.com>
2014-12-16 14:30:25 +0100

    fix 'mem' on macosx

--- ustat_mem.c
+++ ustat_mem.c
@@ -29,31 +29,77 @@ int mem_init(struct ustat_module* m, const char* s, size_t l) {
 
 #if defined(BSD)
 
-#include <sys/sysctl.h>
+#if defined(__APPLE__)
 
-#if defined (CTL_VM)
-#include <vm/vm_param.h>
-#include <sys/vmmeter.h>
-#endif
-
-#include <stdio.h>
+#include <mach/mach.h>
 
 static int _get_total_free(uint64_t* mem_total, uint64_t* mem_free) {
 
-    size_t len;
-    int mib[2];
+    size_t  len;
+    int     mib[2];
+    long    ps = sysconf(_SC_PAGESIZE);
 
-    long total = sysconf(_SC_PHYS_PAGES);
-    long ps = sysconf(_SC_PAGESIZE);
-    if (ps == -1 || total == -1) {
+    struct vm_statistics64 vm;
+    mach_port_t            port = mach_host_self();;
+    mach_msg_type_number_t count = sizeof(vm) / sizeof(natural_t);
+
+    if (host_statistics64(port, HOST_VM_INFO64, (host_info_t)&vm, &count) != KERN_SUCCESS) {
         return 0;
     }
 
-#if defined (CTL_VM) && (defined(VM_TOTAL))
+    *mem_total = (uint64_t)vm.free_count
+               + (uint64_t)vm.active_count
+               + (uint64_t)vm.inactive_count
+               + (uint64_t)vm.wire_count;
+    *mem_total *= (uint64_t)ps;
+
+    *mem_free = (uint64_t)vm.free_count * (uint64_t)ps;
+
+    return 1;
+}
+
+#else // *BSD
+
+#include <sys/sysctl.h>
+#include <sys/vmmeter.h>
+
+static int _get_total_free(uint64_t* mem_total, uint64_t* mem_free) {
+
+    size_t  len;
+    int     mib[2];
+    long    total = 0;
+    long    ps = sysconf(_SC_PAGESIZE);
+
+    if (ps == -1) {
+        return 0;
+    }
+
+#if defined (CTL_HW) && (defined(HW_MEMSIZE) || defined(HW_PHYSMEM64))
+    {
+        int64_t t;
+        mib[0] = CTL_HW;
+#if defined (HW_MEMSIZE)
+        mib[1] = HW_MEMSIZE;
+#elif defined (HW_PHYSMEM64)
+        mib[1] = HW_PHYSMEM64;
+#endif
+        len = sizeof(t);
+        if (sysctl(mib, 2, &t, &len, 0, 0) == 0) {
+            total = t;
+        }
+    }
+#endif // CTL_HW
+
+
+#if defined (CTL_VM) && (defined(VM_TOTAL) || defined(VM_METER))
     {
         struct vmtotal vmi;
         mib[0] = CTL_VM;
+#if defined (VM_TOTAL)
         mib[1] = VM_TOTAL;
+#else
+        mib[1] = VM_METER;
+#endif
         len = sizeof(vmi);
         if (sysctl(mib, 2, &vmi, &len, 0, 0) == 0) {
             *mem_total = (uint64_t)total * (uint64_t)ps;
@@ -63,28 +109,14 @@ static int _get_total_free(uint64_t* mem_total, uint64_t* mem_free) {
     }
 #endif
 
-#if defined (CTL_HW) && (defined(HW_MEMSIZE) || defined(HW_PHYSMEM64))
-    {
-        int64_t size;
-        mib[0] = CTL_HW;
-#if defined (HW_MEMSIZE)
-        mib[1] = HW_MEMSIZE;
-#elif defined (HW_PHYSMEM64)
-        mib[1] = HW_PHYSMEM64;
-#endif
-        len = sizeof(size);
-        if (sysctl(mib, 2, &size, &len, 0, 0) == 0) {
-            *mem_total = (uint64_t)size;
-        }
-
-        return 1;
-    }
-#endif // CTL_HW
+    *mem_total = (uint64_t)total * (uint64_t)ps;
 
     return 0;
 }
 
-#else
+#endif // *BSD
+
+#else  // LINUX (or other)
 
 #include <sys/sysinfo.h>
 
