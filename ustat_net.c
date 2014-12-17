89ab4566734e097fc27ed712d10884e2fac3f2e3
Mathias Gumz <mg@2hoch5.com>
2014-12-17 22:54:36 +0100

    prepare 'ntcp*' on freebsd

--- ustat_net.c
+++ ustat_net.c
@@ -29,6 +29,8 @@ const size_t _n_tcp_states = sizeof(_tcp_counters) / sizeof(_tcp_counters[0]);
 static int process_proc_tcp(int fd, int skip_fields, uint64_t* store, size_t lstore);
 static int print(int fd, uint64_t val);
 
+static int init_tcp_stats();
+
 
 int tcp_init(struct ustat_module* m, const char* s, size_t l) {
 
@@ -41,43 +43,13 @@ int tcp_init(struct ustat_module* m, const char* s, size_t l) {
         return 1;
     }
 
-    fd = open_read("/proc/net/tcp");
-    if (fd < 0) {
+    if (init_tcp_stats() == 0) {
         return 0;
-    }
-
-    inited = 1;
-
-    byte_zero(_tcp_counters, sizeof(_tcp_counters));
-    byte_zero(_tcp4_counters, sizeof(_tcp4_counters));
-    byte_zero(_tcp6_counters, sizeof(_tcp6_counters));
-
-    // tcpv4
-    process_proc_tcp(fd, 3, _tcp4_counters, _n_tcp_states);
-    close(fd);
-
-    for (i = 1; i < _n_tcp_states; i++) {
-        _tcp4_counters[0] += _tcp4_counters[i];
-        _tcp_counters[i] += _tcp4_counters[i];
-    }
-    _tcp_counters[0] = _tcp4_counters[0];
-
-    // tcpv6, if available
-    fd = open_read("/proc/net/tcp6");
-    if (fd > 0) {
-        process_proc_tcp(fd, 3, _tcp6_counters, _n_tcp_states);
-        close(fd);
-        for (i = 1; i < _n_tcp_states; i++) {
-            _tcp6_counters[0] += _tcp6_counters[i];
-            _tcp_counters[i] += _tcp6_counters[i];
-        }
-        _tcp_counters[0] += _tcp6_counters[0];
     }
 
     m->ready = 1;
     return 1;
 }
-
 
 int ntcp_established_print(int fd, struct ustat_module* m, const char* s, size_t l) {
     return print(fd, _tcp_counters[TCP_ESTABLISHED]);
@@ -107,6 +79,12 @@ int static print(int fd, uint64_t val) {
     return 1;
 }
 
+#if defined(BSD)
+
+
+#else
+
+#include <ctype.h>
 
 // ugly state-maschine
 struct sm {
@@ -215,4 +193,46 @@ end:
 
     return rc;
 }
+
+
+static int init_tcp_stats() {
+
+    int i;
+    int fd = open_read("/proc/net/tcp");
+    if (fd < 0) {
+        return 0;
+    }
+
+    inited = 1;
+
+    byte_zero(_tcp_counters, sizeof(_tcp_counters));
+    byte_zero(_tcp4_counters, sizeof(_tcp4_counters));
+    byte_zero(_tcp6_counters, sizeof(_tcp6_counters));
+
+    // tcpv4
+    process_proc_tcp(fd, 3, _tcp4_counters, _n_tcp_states);
+    close(fd);
+
+    for (i = 1; i < _n_tcp_states; i++) {
+        _tcp4_counters[0] += _tcp4_counters[i];
+        _tcp_counters[i] += _tcp4_counters[i];
+    }
+    _tcp_counters[0] = _tcp4_counters[0];
+
+    // tcpv6, if available
+    fd = open_read("/proc/net/tcp6");
+    if (fd > 0) {
+        process_proc_tcp(fd, 3, _tcp6_counters, _n_tcp_states);
+        close(fd);
+        for (i = 1; i < _n_tcp_states; i++) {
+            _tcp6_counters[0] += _tcp6_counters[i];
+            _tcp_counters[i] += _tcp6_counters[i];
+        }
+        _tcp_counters[0] += _tcp6_counters[0];
+    }
+
+    return 1;
+}
+
+#endif
 
