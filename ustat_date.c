599f2bc781230cadb4ff09deeeed67c95fd5b82f
Mathias Gumz <mg@2hoch5.com>
2015-02-12 20:36:54 +0100

    switch to gettimeofday() for getting the current time
    i should have known better in the first place:
    
    http://stackoverflow.com/a/23597725/98653 (my answer) and the code i used in
    fluxbox to correctly show the time use gettimeofday() for better accuracy.
    using time() cuts off the fraction part of the struct that is used in kernel
    land to hold the time. the result is that the rendered time looks ~0.5s behind
    other, more accurat clocks.

--- ustat_date.c
+++ ustat_date.c
@@ -1,14 +1,24 @@
 #include "ustat.h"
 #include "djb/str.h"
-#include <time.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdint.h>
+
+static const uint64_t MICRO_SECS = 1000L * 1000L;
 
 int date_print(int fd, struct ustat_module* m, const char* s, size_t l) {
 
     char buf[256];
-    time_t t = time(0);
-    struct tm* t2 = gmtime(&t);
-    size_t n = strftime(buf, sizeof(buf), &s[str_len(m->name)], t2);
+    struct timeval t;
+    struct tm* t2;
+    size_t n;
+
+    gettimeofday(&t, 0);
+    t.tv_sec = ((t.tv_sec * MICRO_SECS) + t.tv_usec) / MICRO_SECS;
+    t2 = gmtime(&t.tv_sec);
+    n = strftime(buf, sizeof(buf), &s[str_len(m->name)], t2);
+
     if (n > 0) {
         write(fd, buf, n);
     }
